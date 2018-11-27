7b372211ca5b10973bdf6fbde72c1a3732c5ffba
kumar-archit <archit_kumar@students.iitmandi.ac.in>
2018-11-27 15:15:36 +0530

    Update genCase.cpp
    added rajanish's updates

--- genCase.cpp
+++ genCase.cpp
@@ -75,6 +75,24 @@ void prob_adjustment(vector<vector<double> > &transitionMatrix, vector<vector<do
     }
   }
 }
+void prob_adjustment_updated(vector<vector<double> > &transitionMatrix, vector<vector<double> >& inputAlphabetsDistribution, vector<vector<double> > &mutualIm) {
+  int M  = inputAlphabetsDistribution.size();
+  for(int i=0; i<M; i++){
+    for(int j=0; j<2; j++){
+      double num; 
+      double deno=0;
+	  // cout << "vm: " << verti_marginalVals[i][j] << endl;
+      num = exp(mutualIm[i][j]) - 1;
+      for(int k=0; k < 2 ; k++) {
+		  // cout << "inp Dis: " << inputAlphabetsDistribution[i][k] << endl;
+		  deno += (inputAlphabetsDistribution[i][k])*(exp(mutualIm[i][k]) - 1);
+	  }
+      if(deno == 0) cout << "Chutiya: " << endl;
+	  // cout << "Num : " << num << " " << "Deno : " << deno << endl;
+	  inputAlphabetsDistribution[i][j] *= num/deno;
+    }
+  }
+}
 int binToDec(vector<int> rep){
   int temp = rep.size()-1;
   int siz = rep.size();
@@ -122,8 +140,58 @@ void marginalisation( vector<vector<double> > &mutualIm, vector<double> &outputD
      }
 }
 
+void marginalisation_updated( vector<vector<double> > &mutualIm, vector<double> &outputDis, vector<vector<double> > &inputAlphabetsDistribution, vector<vector<double> > &transmissionMatrix){
+  int M = inputAlphabetsDistribution.size();
+  int count;
+  double product;
+  vector<vector<double> > tempDistribution(M-1, vector<double> (2,0));
+	int lim = pow(2, M-1);
+	for(int i = 0; i < M; i++) {
+      count=0;
+      for(int j = 0; j < M; j++) if(j != i){
+        for(int k=0; k<2; k++){
+          tempDistribution[count][k] = inputAlphabetsDistribution[j][k];
+        }
+          count++;
+      } 
+      for(int k=0; k<2; k++){  
+	      double sum = 0;
+      	 for(int j = 0; j < lim; ++j) {
+           vector<int> xm_dash;
+           decToBin(j, M - 1, xm_dash);
+           vector<int> xm = xm_dash;
+           xm.emplace(xm.begin() + i, k);
+           int pos = binToDec(xm);
+           double p_xm_dash=1 ;
+           for(int p=0; p<M-1; p++){
+             p_xm_dash *= tempDistribution[p][xm_dash[p]];
+           }
+           double p_xm = 1;
+           for(int p=0; p<M; p++){
+             p_xm *= inputAlphabetsDistribution[p][xm[p]];
+           }
+           double term = 0;
+           //double contri = 1;
+           for(int y = 0; y < outputDis.size(); ++y) {
+             double p_y_given_x_M = transmissionMatrix[pos][y];
+             double p_y = outputDis[y];
+             term += p_y_given_x_M*log(p_y_given_x_M/p_y);
+
+            //  product *= pow((p_y_given_x_M)/(p_y), p_xm_dash*p_y_given_x_M);
+           }
+		   term *= p_xm_dash;
+		   sum += term;
+         }
+        mutualIm[i][k] = sum;
+       }
+  		
+     }
+}
+
 void maximisation(vector<vector<double> > &transmissionMatrix, vector<vector<double> > &inputAlphabetsDistribution, vector<vector<double> > &mutualIm){
-	prob_adjustment(transmissionMatrix, inputAlphabetsDistribution, mutualIm);	
+// 	prob_adjustment(transmissionMatrix, inputAlphabetsDistribution, mutualIm);	
+	prob_adjustment_updated(transmissionMatrix, inputAlphabetsDistribution, mutualIm);	
+
 }
 
 double em(vector<vector<double> > &inputAlphabetsDistribution, vector<vector<double> > &transmissionMatrix, double error = 0.01, int iterations = 1){
@@ -137,7 +205,8 @@ double em(vector<vector<double> > &inputAlphabetsDistribution, vector<vector<dou
     if(abs(capa - old_c) < error) return capa;
     old_c = capa;
 	  cout << LIM - iterations << " " << capa << endl;
-    marginalisation(mutualIm, outputDis, inputAlphabetsDistribution, transmissionMatrix);
+//     marginalisation(mutualIm, outputDis, inputAlphabetsDistribution, transmissionMatrix);
+    marginalisation_updated(mutualIm, outputDis, inputAlphabetsDistribution, transmissionMatrix);
     maximisation(transmissionMatrix, inputAlphabetsDistribution, mutualIm);
   }
   return capa;
