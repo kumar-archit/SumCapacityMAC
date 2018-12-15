2a0dd6d3952ea550d038c839e2599674dd9fc3a3
kumar-archit <archit_kumar@students.iitmandi.ac.in>
2018-12-15 17:30:51 +0530

    Inserted comments for better understanding

--- genCaseNary.cpp
+++ genCaseNary.cpp
@@ -1,7 +1,27 @@
-//general case arbitrary alphabet size
-//MAC
+/* A program to find the capacity of MAC channel Multi user case (each alphabet is arbitrary size) by implementing Blahut Arimoto Algorithm
+ *	
+ *  Created on: 20-Oct-2018
+ *  Author : Archit Kumar
+ */
+
+/*
+Input[Optional]: 
+1. input probability distribution of input alphabet. 
+2. transition matrix for the channel
+[3]. Number of iterations for Expectation-Maximisation to run.
+[4]. Accepted error in Capacity.
+Output:
+Capacity of the Channel
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
+
+/*
+ * a function to convert a decimal number to a vector, the size equal to inputAlphabetsDisSize.
+ * this function is required to find the row corresponding to the input vector in the transmission matrix. 
+ * See also: binToDec
+ */
 void xM(int x, vector<vector<double> > &inputAlphabetsDistribution, vector<int>& inpVector) {
   int i, siz;
   for(i=0; i<inpVector.size(); i++)inpVector[i]=0;
@@ -14,6 +34,11 @@ void xM(int x, vector<vector<double> > &inputAlphabetsDistribution, vector<int>&
   }
   reverse(inpVector.begin(), inpVector.end());
 }
+
+/* 
+ * converts a vector to a decimal number which translates to the row corresponding to the input vector in the
+ * transition matrix.
+ */`
 int nPerm(vector<int> inpVector, vector<vector<double> > &inputAlphabetsDistribution, int noOfPermutation){
   int siz = inpVector.size();
   int ans=0;
@@ -24,6 +49,12 @@ int nPerm(vector<int> inpVector, vector<vector<double> > &inputAlphabetsDistribu
   }
   return ans;
 }
+
+/*
+ * a function to calculate capacity  by first calculating probability distribution of output alphabet
+ * noOfPermutation	:	total number of possible input vectors 
+ * probY		:	vector to store output alphabet probability distribution
+ */
 double capacity(vector<double> &outputDis, vector<vector<double> > &inputAlphabetsDistribution, vector<vector<double> > &transmissionMatrix){
   //calc P(Y)
 	int noOfPermutation = transmissionMatrix.size();
@@ -58,6 +89,17 @@ double capacity(vector<double> &outputDis, vector<vector<double> > &inputAlphabe
 
 	return capacity;
 }
+
+/*
+ * a function to calculate mutual information of each alphabets' every symbol in a 2d vector mutualIm
+ * tempDistribution is a 2d vector used to store the probability distribution of all alphabets other than the 
+ * currently focussed one in Marginalisation algorithm. This matrix is of size (noOfInputAlphabets-1)*2.
+ *
+ * a temporary vector xm_dash (one less dimension) is generated which can be used to in-turn generate many more  
+ * (equal to size of omitted alphabet).
+ *
+ */
+
 void marginalisation( vector<vector<double> > &mutualIm, vector<double> &outputDis, vector<vector<double> > &inputAlphabetsDistribution, vector<vector<double> > &transmissionMatrix){
 	int noOfInputAlphabets = inputAlphabetsDistribution.size();
 	int count, siz, pos;
@@ -105,6 +147,11 @@ void marginalisation( vector<vector<double> > &mutualIm, vector<double> &outputD
 
 	}
 }
+
+/*
+ * a function to change the probability distributions of input alphabets using 
+ * mutual information. This updation of probability distribution happens in each step of EM method.
+ */
 void prob_adjustment(vector<vector<double> >& inputAlphabetsDistribution, vector<vector<double> > &mutualIm) {
 	int noOfInputAlphabets  = inputAlphabetsDistribution.size();
 	double num, deno;
@@ -121,7 +168,11 @@ void prob_adjustment(vector<vector<double> >& inputAlphabetsDistribution, vector
 		}
 	}
 }
-
+/*
+ * Implementation of Expectation-Maximisation paradigm
+ * Expectation step is performed by marginalisation function
+ * Maximisation step is performed by Maximisation function
+ */
 double em(vector<vector<double> > &inputAlphabetsDistribution, vector<vector<double> > &transmissionMatrix, double error = 0.01, int iterations = 1){
 	double capa, old_c = 0;
 	int noOfInputAlphabets = inputAlphabetsDistribution.size(), outputAlphabetSize=transmissionMatrix[0].size();
@@ -142,7 +193,7 @@ double em(vector<vector<double> > &inputAlphabetsDistribution, vector<vector<dou
 
 	}
 }
-
+//Initialization of inputAlphabetsDistribution vector and transmissionMatrix 2d vector by taking input from user/ file
 void init(int noOfInputAlphabets, int outputAlphabetSize, vector<int> alphabetSize, vector<vector<double> > &inputAlphabetsDistribution, vector<vector<double> > &transmissionMatrix){
 	int  i,j, siz=1, tSiz;
 	alphabetSize.resize(noOfInputAlphabets);
